827d07f9f1bf2e1da8d1393f8afb4778cddc2692
Paul Sastrasinh <psastras@gmail.com>
2018-06-26 07:30:16 -0400

    docs: fix worker_threads comments

--- lib/worker_threads.d.ts
+++ lib/worker_threads.d.ts
@@ -121,11 +121,16 @@ declare module "worker_threads" {
    * Optional properties passed in to the Worker constructor.
    */
   export interface WorkerOptions {
-    eval?: boolean; // If true, interpret the first argument to the constructor as a script that is executed once the worker is online.
-    workerData?: any; // Any JavaScript value that will be cloned and made available as require('worker_threads').workerData. The cloning will occur as described in the HTML structured clone algorithm, and an error will be thrown if the object cannot be cloned (e.g. because it contains functions).
-    stdin?: boolean; // If this is set to true, then worker.stdin will provide a writable stream whose contents will appear as process.stdin inside the Worker. By default, no data is provided.
-    stdout?: boolean; // If this is set to true, then worker.stdout will not automatically be piped through to process.stdout in the parent.
-    stderr?: boolean; // If this is set to true, then worker.stderr will not automatically be piped through to process.stderr in the parent.
+    // If true, interpret the first argument to the constructor as a script that is executed once the worker is online.
+    eval?: boolean;
+    // Any JavaScript value that will be cloned and made available as require('worker_threads').workerData. The cloning will occur as described in the HTML structured clone algorithm, and an error will be thrown if the object cannot be cloned (e.g. because it contains functions).
+    workerData?: any;
+    // If this is set to true, then worker.stdin will provide a writable stream whose contents will appear as process.stdin inside the Worker. By default, no data is provided.
+    stdin?: boolean;
+    // If this is set to true, then worker.stdout will not automatically be piped through to process.stdout in the parent.
+    stdout?: boolean;
+    // If this is set to true, then worker.stderr will not automatically be piped through to process.stderr in the parent.
+    stderr?: boolean;
   }
 
   /**
